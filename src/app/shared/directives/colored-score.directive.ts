2c957f8134e3affdafaf3f6e76e3cf95b4a5d60e
diegoemanceras <diegoemanceras@gmail.com>
2023-08-01 01:36:42 +0200

    (fix) Colored score directive

--- src/app/shared/directives/colored-score.directive.ts
+++ src/app/shared/directives/colored-score.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appColoredScore]'
 })
-export class ColoredScoreDirective {
+export class ColoredScoreDirective implements OnInit {
 
   @Input() appColoredScore = 0;
 
   constructor(private el: ElementRef) {
+  }
+
+  ngOnInit() {
     if(this.appColoredScore >= 4 ) {
       this.el.nativeElement.style.backgroundColor = 'green';
     } else if( this.appColoredScore >=2 && this.appColoredScore < 4) {
